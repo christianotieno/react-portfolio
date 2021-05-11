b9f9a1209ec7b3ee632f5caa87530f58e9afe29a
christianotieno <oduorchristian@gmail.com>
2021-05-11 12:23:28 +0300

    Configure react to use tailwindcss

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar';
 import Landing from '../Landing/Landing';
 
 const App = () => (
-  <div className="App">
+  <div className="App bg-white dark:bg-gray-800">
     <Navbar />
     <Landing />
   </div>
