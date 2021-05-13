31229f1a5e4b6a68d58894f9519f22d059bbc95c
christianotieno <oduorchristian@gmail.com>
2021-05-13 04:16:53 +0300

    finish styling on recent works page

--- src/components/RecentWorks.js
+++ src/components/RecentWorks.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './RecentWorks.css';
 import vectorBar from '../assets/images/Vector.png';
+import roshan from '../assets/images/roshan-game.png';
 
 const RecentWorks = () => (
   <div className="bg-portfolio_mercury recent-works">
@@ -14,29 +15,114 @@ const RecentWorks = () => (
         </div>
       </div>
       <div className="">
-        <div className="border-2">
-          1
+        <div className="grid grid-cols-2">
+          <div className="left-div">
+            <div className="rounded top-project-img">
+              <img src={roshan} alt="" className="rounded" />
+            </div>
+          </div>
+          <div className="right-div rounded ml-10">
+            <h3 className="font-creteRound text-portfolio_downriver text-4xl">
+              Roshan, the treasure hunter.
+            </h3>
+            <div className="mt-8 text-3xl text-portfolio_downriver">
+              This game was inspired by the endless runner game
+              (5-part tutorial). Inspiration was drawn to design
+              a simple but creative game, and implement it using Phaser.
+              Therefore, I did spend some time designing the game before programming it.
+            </div>
+            <div className=" text-2xl tools-div grid grid-cols-4 mt-8">
+              <div className="rounded text-center mr-2 tool-used">Phaser-3</div>
+              <div className="rounded text-center mr-2 tool-used">JavaScript</div>
+              <div className="rounded text-center mr-2 tool-used">Webpack</div>
+              <div className="rounded text-center tool-used">Netlify</div>
+            </div>
+            <div className="rounded text-center project-btn mt-5 text-portfolio_mercury font-bold py-2 px-4">
+              See project
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-3">
-          <div className="border-2">
-            2
+        <div className="my-10 grid grid-cols-3 gap-5">
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen bg-book-store rounded p-10">
+            <div className="bg-portfolio_mercury p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_cod_gray font-creteRound text-4xl">
+              Bookstore App
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">
+              A Bookstore project built
+              with React and Redux and hosted live on Netlify.
+            </div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">React</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Redux</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Node-js</div>
+            </div>
           </div>
-          <div className="border-2">
-            3
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen bg-tic-tac-toe rounded p-10">
+            <div className="bg-portfolio_mercury p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_cod_gray font-creteRound text-4xl">
+              Tic Tac Toe
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">
+              A tic-tac-toe project built
+              with vanilla javascript and tested with the jest framework.
+            </div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">JavaScript</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Node-js</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Jest</div>
+            </div>
           </div>
-          <div className="border-2">
-            4
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen bg-covid-19-tracker rounded p-10">
+            <div className="bg-portfolio_mercury p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_cod_gray font-creteRound text-4xl">
+              React Capstone.
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">
+              A React capstone based project
+              for catalogue of statistics tested with the jest framework.
+            </div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">React</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Netlify</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Html</div>
+            </div>
           </div>
-        </div>
-        <div className="grid grid-cols-3">
-          <div className="border-2">
-            5
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen bg-library-app rounded p-10">
+            <div className="bg-portfolio_mercury p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_cod_gray font-creteRound text-4xl">
+              Library app
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">
+              A library project built
+              with JavaScript and collaboratively (pair-programming).
+            </div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">JavaScript</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Dom</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Html</div>
+            </div>
           </div>
-          <div className="border-2">
-            6
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen bg-schedule-tracker rounded p-10">
+            <div className="bg-portfolio_mercury p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_cod_gray font-creteRound text-4xl">
+              Schedule Tracker
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">
+              A schedule tracker built
+              with React/Redux for tracking tasks inside of schedules from rails API.
+            </div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">React</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Redux</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">Json-API</div>
+            </div>
           </div>
-          <div className="border-2">
-            7
+          <div className="bg-center bg-cover bg-no-repeat shadow-2xl h-half_screen rounded p-10">
+            <div className="bg-portfolio_mineShaft p-2 rounded w-3/4 mx-auto mt-40 text-center text-portfolio_mercury font-creteRound text-4xl">
+              heading
+            </div>
+            <div className="text-2xl text-center text-portfolio_mercury rounded m-2">description</div>
+            <div className="text-portfolio_mercury m-5 grid grid-cols-3 gap-1">
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">button</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">button</div>
+              <div className="bg-portfolio_downriver rounded m-1 p-1 text-center">button</div>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
       portfolio_downriver: '#091e42',
       portfolio_cod_gray: '#1c1a19',
     },
+    height: {
+      half_screen: '50vh',
+    },
     extend: {
+      backgroundImage: (theme) => ({
+        'book-store': "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%),url('./assets/images/book-store.png')",
+        'covid-19-tracker': "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%),url('./assets/images/covid-19-tracker.png')",
+        'library-app': "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%),url('./assets/images/library-app.png')",
+        'schedule-tracker': "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%),url('./assets/images/schedule-tracker.png')",
+        'tic-tac-toe': "linear-gradient(179.35deg, rgba(38, 38, 38, 0) 0.85%, rgba(38, 38, 38, 0.9) 84%),url('./assets/images/tic-tac-toe.png')",
+
+      }),
       fontFamily: {
         inter: ['Inter'],
         creteRound: ['Crete Round'],
